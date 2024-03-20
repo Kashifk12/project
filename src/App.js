7aa545a29c902c7ebce7d960f18741c2b10f680b
Kashifk12 <kashif839293@gmail.com>
2024-03-21 04:44:15 +0500

    feature:create new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
 function App() {
+  const [value, setValue] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Navbar logoText="Kashif" />
+    <div className="value"><h1>{value}</h1></div>
+    <button onClick={()=>{setValue(value + 1)}}>Click me</button>
+    <Footer />
     </div>
   );
 }
